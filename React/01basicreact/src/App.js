ac6c55aab0a4a944841d14b52c84ad3787818cda
Tanushwi <tanushwisingh27@gmail.com>
2026-03-01 10:20:22 +0530

    Day 3 and Day 4 done

--- React/01basicreact/src/App.js
+++ React/01basicreact/src/App.js
@@ -1,8 +1,9 @@
-
+import Chai from './Chai.js'
 function App() {
   return (
     <div>
       <h1>chai aur react | Tanushwi Singh </h1>
+      <Chai/>
     </div>
   );
 }
--- React/01vitereact/src/App.jsx
+++ React/01vitereact/src/App.jsx
@@ -1,7 +1,16 @@
+import Chai from './chai.jsx'
 
 function App() {
+  const username="Tanushwi"
   return (
+    <>
+    <Chai/>
     <h1>Chai aur react with vite | Tanushwi Singh</h1>
+    
+    {/* in curly brackets only evaluated expression can be written - final output agr if vgera kuch b krna h normal kroo {} k andr nhi */}
+
+    <p>Done by {username}</p>  
+    </>
   )
 }
 
